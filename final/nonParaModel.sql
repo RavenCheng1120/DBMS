7f11693310d7cd03b0cb1bfcbabd21290a8a72ee
RavenCheng1120 <ravencheng66@gmail.com>
2021-05-27 02:07:43 +0800

    start friedman

--- final/nonParaModel.sql
+++ final/nonParaModel.sql
@@ -1,54 +1,75 @@
 /***** create and use database *****/
 CREATE DATABASE StatDB;
 USE StatDB;
-SELECT DATABASE();
 
-
-/***** table creation and insertion *****/
-CREATE TABLE TestDataA(
-	UID int NOT NULL,
-	Enjoyment float NOT NULL,
-	Realism float NOT NULL,
-	Immersion float NOT NULL,
-	PRIMARY KEY (UID)
+CREATE TABLE conditionA (
+    UserNum int NOT NULL,
+    Enjoyment int NOT NULL,
+    Realism int NOT NULL,
+    Immersion int NOT NULL,
+    PRIMARY KEY (UserNum)
 );
 
-CREATE TABLE TestDataB(
-	UID int NOT NULL,
-	Enjoyment float NOT NULL,
-	Realism float NOT NULL,
-	Immersion float NOT NULL,
-	PRIMARY KEY (UID)
+CREATE TABLE conditionB (
+    UserNum int NOT NULL,
+    Enjoyment int NOT NULL,
+    Realism int NOT NULL,
+    Immersion int NOT NULL,
+    PRIMARY KEY (UserNum)
 );
 
-INSERT INTO TestDataA VALUES
+CREATE TABLE conditionC (
+    UserNum int NOT NULL,
+    Enjoyment int NOT NULL,
+    Realism int NOT NULL,
+    Immersion int NOT NULL,
+    PRIMARY KEY (UserNum)
+);
+
+INSERT INTO conditionA
+VALUES
 (0, 2, 1, 2),
 (1, 3, 3, 4),
 (2, 4, 4, 4),
 (3, 4, 3, 3),
 (4, 3, 3, 3),
-(5, 3 ,1 ,1),
+(5, 3, 1, 1),
 (6, 4, 4, 4),
 (7, 2, 5, 4),
 (8, 5, 6, 7),
 (9, 3, 3, 3),
-(10 ,4 ,4 ,4),
+(10, 4, 4, 4),
 (11, 4, 1, 1);
 
-INSERT INTO TestDataB VALUES
+INSERT INTO conditionB
+VALUES
 (0, 5, 5, 6),
 (1, 4, 4, 4),
 (2, 1, 2, 3),
 (3, 5, 5, 5),
 (4, 4, 5, 5),
-(5, 4 ,2 ,2),
+(5, 4, 2, 2),
 (6, 5, 4, 4),
 (7, 1, 5, 6),
 (8, 2, 5, 5),
 (9, 5, 6, 6),
-(10 ,7 ,7 ,7),
+(10, 7, 7, 7),
 (11, 4, 3, 3);
 
+INSERT INTO conditionC
+VALUES
+(0, 6, 6, 6),
+(1, 6, 6, 6),
+(2, 6, 5, 6),
+(3, 6, 6, 6),
+(4, 6, 6, 6),
+(5, 5, 6, 6),
+(6, 6, 6, 6),
+(7, 4, 3, 5),
+(8, 4, 5, 7),
+(9, 7, 6, 6),
+(10, 7, 6, 6),
+(11, 5, 3, 3);
 
 /***** Store Function and Store Procedure *****/
 -- Calculate difference between two values
@@ -63,7 +84,7 @@ END //
 
 -- Wilconxon algorithm
 DROP PROCEDURE IF EXISTS `Wilcoxon`//
-CREATE PROCEDURE `Wilcoxon`(IN table1 varchar(25), IN table2 varchar(25), IN columnName1 varchar(25), IN columnName2 varchar(25))
+CREATE PROCEDURE `Wilcoxon`(IN uid varchar(25), IN table1 varchar(25), IN table2 varchar(25), IN columnName1 varchar(25), IN columnName2 varchar(25))
 	BEGIN
 		-- set up variables
 		DECLARE totalRowNumber INT DEFAULT 1;
@@ -73,24 +94,25 @@ CREATE PROCEDURE `Wilcoxon`(IN table1 varchar(25), IN table2 varchar(25), IN col
 		DROP TEMPORARY TABLE IF EXISTS `TempTable`;
 		DROP TABLE IF EXISTS `RankTable`;
 		CREATE TABLE RankTable(
-			UID INT NOT NULL,
+			UserID int NOT NULL,
 			Difference float,
 			Absolute float,
 			Ranking float,
-			PRIMARY KEY(UID)
+			PRIMARY KEY(UserID)
 		);
 
 		-- Get difference between column1 and column2
-		SET @statement1 =CONCAT('CREATE TEMPORARY TABLE TempTable SELECT t1.UID, t1.', columnName1, ' AS column1, t2.', columnName2, ' AS column2, Diff(t1.', columnName1, ', t2.', columnName2, ') AS difference FROM ', table1, ' AS t1, ', table2, ' AS t2 WHERE t1.UID = t2.UID;');
+		SET @statement1 = CONCAT('CREATE TEMPORARY TABLE TempTable SELECT t1.', uid ,' AS UserID, t1.', columnName1, ' AS column1, t2.', columnName2, ' AS column2, Diff(t1.', columnName1, ', t2.', columnName2, ') AS difference FROM ', table1, ' AS t1, ', table2, ' AS t2 WHERE t1.', uid ,' = t2.', uid ,';');
 		PREPARE stmt1 FROM @statement1;
 		EXECUTE stmt1;
 		DEALLOCATE PREPARE stmt1;
 
 		-- Get absolute difference value & remove 0
-		INSERT INTO RankTable(UID, Difference, Absolute)
-		SELECT UID, Difference, ABS(Difference)
+		INSERT INTO RankTable(UserID, Difference, Absolute)
+		SELECT UserID, Difference, ABS(Difference)
 		FROM TempTable
 		WHERE ABS(Difference) > 0;
+
 		DROP TEMPORARY TABLE TempTable;
 
 		ALTER TABLE RankTable ORDER BY Absolute ASC;
@@ -138,11 +160,38 @@ CREATE PROCEDURE `Wilcoxon`(IN table1 varchar(25), IN table2 varchar(25), IN col
 		DROP TEMPORARY TABLE TempTable;
 		DROP TABLE RankTable;
 	END//
+
+
+-- Friedman's ANOVA
+DROP PROCEDURE IF EXISTS `Friedman`//
+CREATE PROCEDURE `Friedman`(IN uid varchar(25), IN table1 varchar(25), IN table2 varchar(25), IN table3 varchar(25), IN columnName1 varchar(25), IN columnName2 varchar(25), IN columnName3 varchar(25))
+	BEGIN
+		DROP TEMPORARY TABLE IF EXISTS `TempTable`;
+		DROP TABLE IF EXISTS `RankTable`;
+		CREATE TABLE RankTable(
+			UserID int NOT NULL,
+			Difference float,
+			Absolute float,
+			Ranking float,
+			PRIMARY KEY(UserID)
+		);
+
+		-- Get difference between column1 and column2
+		SET @statement2 = CONCAT('CREATE TEMPORARY TABLE TempTable SELECT t1.', uid ,' AS UID, t1.', columnName1, ' AS column1, t2.', columnName2, ' AS column2, t3.', columnName3, ' AS column3 FROM ', table1, ' AS t1, ', table2, ' AS t2, ', table3, ' AS t3 WHERE t1.', uid ,' = t2.', uid ,' AND t1.', uid ,' = t3.', uid ,';');
+		PREPARE stmt2 FROM @statement2;
+		EXECUTE stmt2;
+		DEALLOCATE PREPARE stmt2;
+
+		SELECT *
+		FROM TempTable;
+
+	END//
 DELIMITER ;
 
 
 /***** main *****/
-CALL Wilcoxon('TestDataA', 'TestDataB', 'Enjoyment', 'Enjoyment');
+-- CALL Wilcoxon('UserNum', 'conditionA', 'conditionC', 'Enjoyment', 'Enjoyment');
+CALL Friedman('UserNum', 'conditionA', 'conditionB', 'conditionC', 'Enjoyment', 'Enjoyment', 'Enjoyment');
 
 
 /***** drop database *****/
